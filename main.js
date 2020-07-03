0bc7de15fdf8532b5c20205d9db7102b1800ff4b
shalinibasu <52671081+shalinibasu@users.noreply.github.com>
2020-07-03 18:23:52 -0400

    Add files via upload
    First Commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// function myMap() {
+// var mapProp= {
+//   center:new google.maps.LatLng(51.508742,-0.120850),
+//   zoom:5,
+// };
+// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// }
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
